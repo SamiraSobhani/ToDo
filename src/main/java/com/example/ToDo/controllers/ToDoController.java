025a7a91296e6a479f7a559d164cc02136060b2b
Samira Sobhani <ss121280@yahoo.com>
2020-11-17 11:20:04 -0600

    base class has been added

--- src/main/java/com/example/ToDo/controllers/ToDoController.java
+++ src/main/java/com/example/ToDo/controllers/ToDoController.java
@@ -1,4 +1,25 @@
 package com.example.ToDo.controllers;
 
+import com.example.ToDo.data.ToDoDao;
+import com.example.ToDo.models.ToDo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
 public class ToDoController {
+
+    private final ToDoDao dao;
+
+    public ToDoController(ToDoDao dao) {
+        this.dao = dao;
+    }
+
+    @GetMapping
+    public List<ToDo> all() {
+        return dao.getAll();
+    }
 }
---
+++ src/main/java/com/example/ToDo/data/ToDoDao.java
@@ -0,0 +1,19 @@
+package com.example.ToDo.data;
+
+import com.example.ToDo.models.ToDo;
+
+import java.util.List;
+
+public interface ToDoDao {
+    ToDo add(ToDo todo);
+
+    List<ToDo> getAll();
+
+    ToDo findById(int id);
+
+    // true if item exists and is updated
+    boolean update(ToDo todo);
+
+    // true if item exists and is deleted
+    boolean deleteById(int id);
+}
