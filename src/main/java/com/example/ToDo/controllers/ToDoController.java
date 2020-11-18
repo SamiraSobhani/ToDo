9cf66d221809b020173356ea23cc7ea5703bf0aa
Samira Sobhani <ss121280@yahoo.com>
2020-11-17 19:45:10 -0600

    complete the controller methods

--- src/main/java/com/example/ToDo/controllers/ToDoController.java
+++ src/main/java/com/example/ToDo/controllers/ToDoController.java
@@ -2,9 +2,9 @@ package com.example.ToDo.controllers;
 
 import com.example.ToDo.data.ToDoDao;
 import com.example.ToDo.models.ToDo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,39 @@ public class ToDoController {
     @GetMapping
     public List<ToDo> all() {
         return dao.getAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ToDo create(@RequestBody ToDo todo) {
+        return dao.add(todo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ToDo> findById(@PathVariable int id) {
+        ToDo result = dao.findById(id);
+        if (result == null) {
+            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable int id, @RequestBody ToDo todo) {
+        ResponseEntity response = new ResponseEntity(HttpStatus.NOT_FOUND);
+        if (id != todo.getId()) {
+            response = new ResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY);
+        } else if (dao.update(todo)) {
+            response = new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable int id) {
+        if (dao.deleteById(id)) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 }
